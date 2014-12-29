6110b62cbff98930faadc1d4611bcf7fb737dfd0
Kris Quigley <kris@krisquigley.co.uk>
2014-12-29 22:41:09 +0000

    Changed how zipped compilations are determined

--- app/controllers/refinery/talks/compilations_controller.rb
+++ app/controllers/refinery/talks/compilations_controller.rb
@@ -8,6 +8,11 @@ module Refinery
         @compilation = Compilation.friendly.find(params[:id])
         @talks = @compilation.talks.order('date ASC').paginate :page => params[:page],
         :per_page => 10
+        @compilation.talks.each do |talk|
+          if File.extname(talk.file.file_uid).downcase == '.zip'
+            @zip = talk
+          end
+        end
       end
 
       protected
--- app/helpers/refinery/talks/mp3_talk_helper.rb
+++ app/helpers/refinery/talks/mp3_talk_helper.rb
@@ -4,10 +4,6 @@ module Refinery
       def show_mp3s(talk)
         File.extname(talk.file.file_uid).downcase == '.mp3'
       end
-
-      def show_zip(talk)
-        File.extname(talk.file.file_uid).downcase == '.zip'
-      end
     end
   end
 end
